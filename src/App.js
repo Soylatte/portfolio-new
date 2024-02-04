e46f61d475afe32c9aaa3085618cdf8559ea90ce
Soyeon Kim <kksoyeon61@gmail.com>
2024-02-04 11:58:38 +0100

    add via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Hero from './components/hero';
 import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar />
+      <Routes>
+        <Route path='/' />
+        <Route path='/skills'/>
+        <Route path='/projects'/>
+        <Route path='/contact'/>
+      </Routes>
       <Hero />
-    </div>
+      </Router>
+      </div>
   );
 }
 
